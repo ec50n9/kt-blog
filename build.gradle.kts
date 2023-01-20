a605b632c714468378c38d446f5173165d701f75
ec50n9 <shelloworld@qq.com>
2023-01-20 14:55:04 +0800

    增加nanoid生成工具

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.auth0:java-jwt:4.2.1")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.0.1")
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
+    implementation("com.soywiz.korlibs.krypto:krypto:3.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
