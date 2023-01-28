7df3fb0411f056d29760718a5601daf0af71a0b9
ec50n9 <shelloworld@qq.com>
2023-01-28 10:06:58 +0800

    将jwt的token名称放到配置文件中

--- src/main/kotlin/com/example/blog/BlogProperties.kt
+++ src/main/kotlin/com/example/blog/BlogProperties.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
 @ConfigurationProperties("blog")
-data class BlogProperties(var title: String, val banner: Banner) {
+data class BlogProperties(var title: String, val banner: Banner, val jwt: JWT) {
     data class Banner(val title: String? = null, val content: String)
+    data class JWT(val tokenName: String = "token")
 }
