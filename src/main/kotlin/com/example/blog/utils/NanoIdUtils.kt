28e1091f3dfd9c3bdc4d9024aa8a4f3f5e84f9ff
ec50n9 <shelloworld@qq.com>
2023-01-28 17:11:02 +0800

    将NanoIdUtils切换为object

--- src/main/kotlin/com/example/blog/utils/NanoIdUtils.kt
+++ src/main/kotlin/com/example/blog/utils/NanoIdUtils.kt
@@ -7,45 +7,42 @@ import kotlin.math.floor
 import kotlin.math.log
 import kotlin.random.Random
 
-class NanoIdUtils private constructor() {
+object NanoIdUtils {
+    private val DEFAULT_NUMBER_GENERATOR = SecureRandom
 
-    companion object {
-        private val DEFAULT_NUMBER_GENERATOR = SecureRandom
+    private val DEFAULT_ALPHABET: CharArray =
+        "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
 
-        private val DEFAULT_ALPHABET: CharArray =
-            "_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
+    private const val DEFAULT_SIZE = 21
 
-        private const val DEFAULT_SIZE = 21
+    fun randomNanoId(
+        random: Random = DEFAULT_NUMBER_GENERATOR,
+        alphabet: CharArray = DEFAULT_ALPHABET,
+        size: Int = DEFAULT_SIZE
+    ): String {
+        if (alphabet.isEmpty() || alphabet.size >= 256) {
+            throw IllegalArgumentException("Alphabet must contain between 1 and 255 symbols.")
+        }
+        if (size <= 0) {
+            throw IllegalArgumentException("Size must be greater than zero.")
+        }
 
-        fun randomNanoId(
-            random: Random = DEFAULT_NUMBER_GENERATOR,
-            alphabet: CharArray = DEFAULT_ALPHABET,
-            size: Int = DEFAULT_SIZE
-        ): String {
-            if (alphabet.isEmpty() || alphabet.size >= 256) {
-                throw IllegalArgumentException("Alphabet must contain between 1 and 255 symbols.")
-            }
-            if (size <= 0) {
-                throw IllegalArgumentException("Size must be greater than zero.")
-            }
+        val mask: Int = (2 shl floor(log((alphabet.size - 1).toDouble(), 2.0)).toInt()) - 1
+        val step: Int = ceil(1.6 * mask * size / alphabet.size).toInt()
+        val idBuilder = StringBuilder()
 
-            val mask: Int = (2 shl floor(log((alphabet.size - 1).toDouble(), 2.0)).toInt()) - 1
-            val step: Int = ceil(1.6 * mask * size / alphabet.size).toInt()
-            val idBuilder = StringBuilder()
+        while (true) {
+            val bytes = ByteArray(step)
+            random.nextBytes(bytes)
 
-            while (true) {
-                val bytes = ByteArray(step)
-                random.nextBytes(bytes)
+            for (i in 0 until step) {
+                val alphabetIndex: Int = (bytes[i] and mask.toByte()).toInt()
 
-                for (i in 0 until step) {
-                    val alphabetIndex: Int = (bytes[i] and mask.toByte()).toInt()
+                if (alphabetIndex < alphabet.size) {
+                    idBuilder.append(alphabet[alphabetIndex])
 
-                    if (alphabetIndex < alphabet.size) {
-                        idBuilder.append(alphabet[alphabetIndex])
-
-                        if (idBuilder.length == size) {
-                            return idBuilder.toString()
-                        }
+                    if (idBuilder.length == size) {
+                        return idBuilder.toString()
                     }
                 }
             }
