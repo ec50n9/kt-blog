340c2c828b57dffcebefe5d2ef330c0b3daa8a6e
ec50n9 <shelloworld@qq.com>
2023-01-28 16:52:41 +0800

    对LoginAspect的功能进行测试

--- src/main/kotlin/com/example/blog/controller/TestController.kt
+++ src/main/kotlin/com/example/blog/controller/TestController.kt
@@ -1,16 +1,21 @@
 package com.example.blog.controller
 
+import com.example.blog.annotation.CurrentUser
 import com.example.blog.annotation.LoginRequired
 import com.example.blog.annotation.PermissionCheck
 import com.example.blog.annotation.RoleCheck
 import com.example.blog.domain.CommonResponse
+import com.example.blog.domain.User
+import com.example.blog.domain.dto.UserViewDto
+import com.example.blog.domain.mapper.UserMapper
+import com.example.blog.service.AuthService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/test")
-class TestController {
+class TestController(private val authService: AuthService, private val userMapper: UserMapper) {
 
     @LoginRequired
     @PermissionCheck(["test", "hello"], false)
@@ -18,5 +23,16 @@ class TestController {
     @GetMapping("/hello")
     fun hello(): CommonResponse<String> {
         return CommonResponse.ok("hello")
+    }
+
+    @LoginRequired
+    @GetMapping("/isLogin")
+    fun needLogin(@CurrentUser user: User): CommonResponse<String> {
+        return CommonResponse.ok("你已经登录啦！！！")
+    }
+
+    @GetMapping("/user")
+    fun getUser(@CurrentUser user: User): CommonResponse<UserViewDto> {
+        return CommonResponse.ok(userMapper.toDto(user))
     }
 }
