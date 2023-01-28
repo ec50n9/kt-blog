888f30ee07ad3547f278fb257a5f5314457978fd
ec50n9 <shelloworld@qq.com>
2023-01-28 10:11:39 +0800

    增加jwt配置

--- src/main/kotlin/com/example/blog/BlogProperties.kt
+++ src/main/kotlin/com/example/blog/BlogProperties.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
 @ConfigurationProperties("blog")
-data class BlogProperties(var title: String, val banner: Banner, val jwt: JWT) {
+data class BlogProperties(var title: String, val banner: Banner, val jwt: JWT = JWT()) {
     data class Banner(val title: String? = null, val content: String)
     data class JWT(val tokenName: String = "token")
 }
