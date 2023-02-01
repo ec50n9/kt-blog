4a08f40baf5c9e62626a419854bce527b480ba3a
ec50n9 <shelloworld@qq.com>
2023-02-01 10:54:00 +0800

    去除多余注释

--- src/main/kotlin/com/example/blog/repo/ArticleRepository.kt
+++ src/main/kotlin/com/example/blog/repo/ArticleRepository.kt
@@ -7,15 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface ArticleRepository : JpaRepository<Article, String> {
     fun findAllByOrderByAddedAtDesc(): Iterable<Article>
-
-    //    @Query("select a from Article a where upper(a.title) like upper(?1)")
     fun findByTitleLikeIgnoreCase(title: String, pageable: Pageable): Page<Article>
-
-    //    @Query("select a from Article a where upper(a.title) like upper(?1) and upper(a.author.username) like upper(?2)")
     fun findByTitleLikeIgnoreCaseAndAuthor_UsernameLikeIgnoreCase(
         title: String?,
         username: String?,
         pageable: Pageable
     ): Page<Article>
-
 }
