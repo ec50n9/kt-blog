667676effc369259c45a6d65653208720c324118
ec50n9 <shelloworld@qq.com>
2023-02-01 10:38:38 +0800

    UserRepository改用JpaRepository实现

--- src/main/kotlin/com/example/blog/repo/UserRepository.kt
+++ src/main/kotlin/com/example/blog/repo/UserRepository.kt
@@ -1,16 +1,13 @@
 package com.example.blog.repo
 
 import com.example.blog.domain.User
-import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
 
-interface UserRepository : CrudRepository<User, String> {
+interface UserRepository : JpaRepository<User, String> {
 
     fun findByUsername(username: String): User?
 
     @Query("select (count(u) > 0) from User u where u.username = ?1")
     fun existsByUsername(username: String): Boolean
-
-    fun findAll(pageable: Pageable): List<User>
 }
